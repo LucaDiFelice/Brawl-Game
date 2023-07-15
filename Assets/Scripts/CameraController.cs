027392a4042c9d6e7c14490b7dfe7a0db29a1b59
Luca Di Felice <Luca.difelice@hts.on.ca>
2023-07-15 12:05:57 -0400

    looking around
    I programmed the camera and now the player can look around.  I also added a plane on the client side so the user can see the plane.  If I just added the plane on the client slide, the user would fall right through the plane

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Player player;
+    [SerializeField] private float sensitivity = 100f;
+    [SerializeField] private float clampAngle = 85f;
+
+    private float verticalRotation;
+    private float horizontalRotation;
+
+    private void OnValidate()
     {
-        
+        if (player == null)
+            player = GetComponentInParent<Player>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        verticalRotation = transform.localEulerAngles.x;
+        horizontalRotation = player.transform.eulerAngles.y;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            ToggleCursorMode();
+
+        if (Cursor.lockState == CursorLockMode.Locked)
+            Look();
+
+        Debug.DrawRay(transform.position, transform.forward * 2f, Color.green);
+    }
+
+    private void Look()
+    {
+        float mouseVertical = -Input.GetAxis("Mouse Y");
+        float mouseHorizontal = Input.GetAxis("Mouse X");
+
+        verticalRotation += mouseVertical * sensitivity * Time.deltaTime;
+        horizontalRotation += mouseHorizontal * sensitivity * Time.deltaTime;
+
+        verticalRotation = Mathf.Clamp(verticalRotation, -clampAngle, clampAngle);
+
+        transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+        player.transform.rotation = Quaternion.Euler(0f, horizontalRotation, 0f);
+    }
+
+    private void ToggleCursorMode()
+    {
+        Cursor.visible = !Cursor.visible;
+
+        if (Cursor.lockState == CursorLockMode.None)
+            Cursor.lockState = CursorLockMode.Locked;
+        else
+            Cursor.lockState = CursorLockMode.None;
     }
 }
