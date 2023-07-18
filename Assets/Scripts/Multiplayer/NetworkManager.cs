e9092114cc0e9a52f703f12489dd0589823f9dda
Luca Di Felice <Luca.difelice@hts.on.ca>
2023-07-18 11:43:50 -0400

    fix up a few things
    had to fix up a few small things

--- Assets/Scripts/Multiplayer/NetworkManager.cs
+++ Assets/Scripts/Multiplayer/NetworkManager.cs
@@ -87,11 +87,14 @@ public class NetworkManager : MonoBehaviour
 
     private void PlayerLeft(object sender, ClientDisconnectedEventArgs e)
     {
-        Destroy(Player.list[e.Id].gameObject);
+        if (Player.list.TryGetValue(e.Id, out Player player))
+            Destroy(player.gameObject);
     }
 
     private void DidDisconnect(object sender, EventArgs e)
     {
         UIManager.Singleton.BackToMain();
+        foreach (Player player in Player.list.Values)
+            Destroy(player.gameObject);
     }
 }
