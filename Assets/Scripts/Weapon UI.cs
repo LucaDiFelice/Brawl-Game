2816bdcd10b54da47ea553d5b5396f9b23210b0a
Luca Di Felice <Luca.difelice@hts.on.ca>
2023-08-21 13:26:00 -0400

    Coded the weapon UI

--- Assets/Scripts/Weapon UI.cs
+++ Assets/Scripts/Weapon UI.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class WeaponUI : MonoBehaviour
 {
-    
+    [SerializeField] private Text text;
+    [SerializeField] private Image background;
+
+    private void OnValidate()
+    {
+        if (text == null)
+            text = GetComponentInChildren<Text>();
+        if (background == null)
+            background = GetComponent<Image>();
+    }
+
+    public void SetActive(bool value)
+    {
+        background.enabled = value;
+    }
+
+    public void UpdateAmmo(byte loaded, ushort total)
+    {
+        text.text = $"{loaded} / {total}";
+    }
+
 }
